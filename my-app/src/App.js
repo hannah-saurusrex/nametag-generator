35975a39596cfd9254c3560f80ff993668194792
hannah-saurusrex <hrose.mcmahon@gmail.com>
2020-02-27 12:06:14 -0700

    refined nametag font styles

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -29,7 +29,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Name Tag Generator</h1>
+        <h1 className="App__title">React Name Tag Generator</h1>
         <UserInput addName={this.addName} />
         <NameTagList names={this.state.names} removeName={this.removeName} />
       </div>
